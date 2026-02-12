9d3ffd747ac6a3c6b5ac48e8a6efeed701c2be82
Sadashrayr <sadashray2003@gmail.com>
2026-02-12 17:27:50 +0530

    Bfs question, average level

---
+++ Trees/BinaryTree/BFS.java
@@ -0,0 +1,20 @@
+package Trees.BinaryTree;
+
+public class BFS {
+    public class Node {
+        private int value;
+        private Node right;
+        private Node left;
+
+        public Node(int value) {
+            value = this.value;
+        }
+    }
+
+    private Node root;
+
+    public BFS() {
+
+    }
+
+}
