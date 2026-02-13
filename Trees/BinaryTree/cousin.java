349a04184c3936594dff489ac93bb8d87611d2db
Sadashrayr <sadashray2003@gmail.com>
2026-02-13 16:38:50 +0530

    not a complete solution

---
+++ Trees/BinaryTree/cousin.java
@@ -0,0 +1,26 @@
+package Trees.BinaryTree;
+
+public class cousin {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public boolean isCousins(TreeNode root, int x, int y) {
+        
+    }
+    
+}
