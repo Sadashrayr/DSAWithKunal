d84ce9e72f9b0e3a0168640631d8c6ea03f5f96a
Sadashrayr <sadashray2003@gmail.com>
2026-02-26 13:08:44 +0530

    Countsort completeded with hashmap

--- Sorting/Countsort.java
+++ Sorting/Countsort.java
@@ -1,6 +1,7 @@
 package Sorting;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Countsort {
     public static void countSort(int[] arr) {
@@ -27,9 +28,30 @@ public class Countsort {
         }
 
     }
+    // array one is recommended
+    public void countSortHash(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+        int max = Arrays.stream(arr).max().getAsInt();
+        int min = Arrays.stream(arr).min().getAsInt();
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        for (int num : arr) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        int index = 0;
+        for (int i = min; i <= max; i++) {
+            int count = countMap.getOrDefault(i, 0);
+            for (int j = 0; j < count; j++) {
+                arr[index] = i;
+                index++;
+            }
+        }
+
+    }
 
     public static void main(String[] args) {
-        int[] arr = { 3, 5, 10, 9, 2, 4, 9, 7,7 };
+        int[] arr = { 3, 5, 10, 9, 2, 4, 9, 7, 7 };
         countSort(arr);
         System.out.println(Arrays.toString(arr));
 
