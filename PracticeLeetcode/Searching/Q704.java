f6c1c6d3c231f9726768053101cf0e0379eb8595
Sadashrayr <sadashray2003@gmail.com>
2026-02-28 15:33:40 +0530

    radix sort

---
+++ PracticeLeetcode/Searching/Q704.java
@@ -0,0 +1,19 @@
+package PracticeLeetcode.Searching;
+
+public class Q704 {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end + start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+    }
+
+}
--- Sorting/Radixsort.java
+++ Sorting/Radixsort.java
@@ -10,15 +10,32 @@ public class Radixsort {
             countSort(arr, exp);
         }
     }
-    private static void countSort(int[] arr,int exp){
-        int n=arr.length;
-        int[] output=new int[n];
-        int[] count=new int[10];
-        Arrays.fill(count,0);
-        
-    }
-    private public static void main(String[] args) {
 
+    private static void countSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+        Arrays.fill(count, 0);
+        for (int i = 0; i < n; i++) {
+            count[(arr[i] / exp) % 10]++;
+        }
+        System.out.println("Count array for" + exp + ":" + Arrays.toString(count));
+        for (int i = 1; i < 10; i++) {
+            count[i] = count[i] + count[i - 1];
+        }
+        System.out.println("Updated Count array " + Arrays.toString(count));
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+        System.arraycopy(output, 0, arr, 0, n);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 29, 83, 471, 36, 91, 8 };
+        System.out.println("Original array: " + Arrays.toString(arr));
+        radixSort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
     }
 
 }
